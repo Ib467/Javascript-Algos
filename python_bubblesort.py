d99c9095862e16fecd85fc0786f5f0b132d6b1d2
Indi Boteju <indiboteju@Indis-MBP.lan>
2020-10-15 13:35:39 -0700

    updated python bubblesort

---
+++ python_bubblesort.py
@@ -0,0 +1,12 @@
+arr =[1,5,3,2,0,8]
+
+def bubbleSort(arr):
+    for i in range(len(arr)):
+        print("\n", "*"*80, "\nindex ", i, "value", arr[i])
+        print("comparing", arr[i], arr[i+1])
+        if arr[i] > arr[i+1]:
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+            print("swapped", arr[i], arr[i+1])
+        else:
+            print("no need to swap". arr[i], arr[i+1])
+bubbleSort(arr)
