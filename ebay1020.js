438b3cffaf67f25ad38e06d739c4355ad02578db
Indi Boteju <indiboteju@Indis-MBP.lan>
2020-10-21 16:16:20 -0700

    code question

---
+++ ebay1020.js
@@ -0,0 +1,4 @@
+var arr = ["Daisy", "Rose", "Hyacinth", "Poppy"];
+//simple interview question by ebay 
+//combine the First letter of each word, then second letter, so forth...
+// output => DRHPaoypisa
