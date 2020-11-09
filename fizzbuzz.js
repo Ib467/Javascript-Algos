4c1070356a32736d40412f6314899bfa252e17e8
Indi Boteju <indiboteju@Indis-MBP.lan>
2020-11-08 21:31:11 -0800

    updated algo

---
+++ fizzbuzz.js
@@ -0,0 +1,9 @@
+function fizzBuzz(n) {
+
+    for (var i=1; i<100; i++){
+        var f = i %3 == 0, b = i%5 ==0;
+        console.log(f ? b ? "FizzBuzz" : "Fizz" : b ? "Buzz" : i);
+    }
+}
+
+fizzBuzz(15);
